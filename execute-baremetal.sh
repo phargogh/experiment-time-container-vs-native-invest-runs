6f1ca1a3363e005cced250af3260dd9cff199e5b
James Douglass <jdouglass@stanford.edu>
2025-07-02 15:04:28 -0700

    Fleshing out baremetal script.

--- execute-baremetal.sh
+++ execute-baremetal.sh
@@ -1,6 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jdouglass@stanford.edu
+#SBATCH --partition=serc,hns,normal
+#SBATCH --job-name="InVEST SDR - Bare Metal Install"
+#SBATCH --output=/scratch/users/jadoug06/slurm-logfiles/slurm-%j.%x.out
 
-# TODO: add sbatch info and invocation.
+set -ex
 
+source invest-env/bin/activate
+
+which python
+
+python execute-invest.py
 
 
