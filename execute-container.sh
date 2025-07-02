5d1ec0c0ad1f72c998143783c91bbad6cc55cf79
James Douglass <jdouglass@stanford.edu>
2025-07-02 15:04:37 -0700

    Adding container execution script.

---
+++ execute-container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jdouglass@stanford.edu
+#SBATCH --partition=serc,hns,normal
+#SBATCH --job-name="InVEST SDR - Singularity/Conda"
+#SBATCH --output=/scratch/users/jadoug06/slurm-logfiles/slurm-%j.%x.out
+
+set -ex
+
+singularity run "docker://ghcr.io/natcap/devstack:2025-07-01" execute-invest.py
